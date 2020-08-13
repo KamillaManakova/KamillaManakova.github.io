f9f5e686aae09fd810d073e9b624b7853a82de3b
KamillaManakova <kamillamanakova@yandex.ru>
2020-08-13 13:41:32 +0600

    Updates on popup form

---
+++ source/js/popup.js
@@ -0,0 +1,7 @@
+function openForm() {
+  document.getElementById("popup__display").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("popup__display").style.display = "none";
+}

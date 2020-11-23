1957b1e2cc3d3a6de3cfedfb817ab5d4edb14fc1
KamillaManakova <kamillamanakova@yandex.ru>
2020-11-23 15:18:38 +0600

    Правки по баглисту-8.3

--- source/js/main-webpcheck.js
+++ source/js/main-webpcheck.js
@@ -10,3 +10,42 @@ function testWebP(elem) {
   }
   console.log(webP)
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  testWebP(document.querySelector(".services__production"))
+})
+
+function testWebP(elem) {
+  const webP = new Image();
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  webP.onload = webP.onerror = function () {
+    webP.height === 2 ? elem.classList.add("webp-true") : elem.classList.add("webp-false")
+  }
+  console.log(webP)
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  testWebP(document.querySelector(".services__montage"))
+})
+
+function testWebP(elem) {
+  const webP = new Image();
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  webP.onload = webP.onerror = function () {
+    webP.height === 2 ? elem.classList.add("webp-true") : elem.classList.add("webp-false")
+  }
+  console.log(webP)
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  testWebP(document.querySelector(".services__complect"))
+})
+
+function testWebP(elem) {
+  const webP = new Image();
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  webP.onload = webP.onerror = function () {
+    webP.height === 2 ? elem.classList.add("webp-true") : elem.classList.add("webp-false")
+  }
+  console.log(webP)
+}
